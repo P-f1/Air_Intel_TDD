3834bde76dab2541d36fc1c20a67352267c5e92c
Infospecktra <Infospecktra@gmail.com>
2021-08-18 09:16:17 -0400

    Update docker-compose.sh

--- deploy/docker-compose.sh
+++ deploy/docker-compose.sh
@@ -17,7 +17,9 @@ function deploy {
 
 	cp -R $Descriptor ./$DeployType/$ServiceName
 	
-	if [ -f "$ArtifactsFolder" ]; then
+	if [[ -f "$ArtifactsFolder" ]]; then
+#		cp -R /home/f1/projects/Air_Intel_TDD/artifacts/vap/* docker/vap
+		echo "Extra resource found!!!"
 		cp -R $ArtifactsFolder/* ./$DeployType/$ServiceName
 	fi
 
